23aa13fcf393bbc57a74aa07518dec70cfeb9b02
Lachlan Charlick <lachlan.charlick@gmail.com>
2025-07-11 17:51:36 +0930

    Make bookmark titles required

--- Sources/AudiobookshelfKit/Requests/UpdateBookmark.swift
+++ Sources/AudiobookshelfKit/Requests/UpdateBookmark.swift
@@ -19,13 +19,13 @@ public extension Audiobookshelf.Request {
 
         private let libraryItemId: String
         private let time: TimeInterval
-        private let title: String?
+        private let title: String
 
         /// - Parameters:
         ///   - libraryItemId: The ID of the library item.
         ///   - time: The time (in seconds) in the book where the bookmark is.
         ///   - title: The new title of the bookmark.
-        public init(libraryItemId: String, time: TimeInterval, title: String?) {
+        public init(libraryItemId: String, time: TimeInterval, title: String) {
             self.libraryItemId = libraryItemId
             self.time = time
             self.title = title
@@ -38,6 +38,6 @@ public extension Audiobookshelf.Request.UpdateBookmark {
 
     struct RequestBody: Codable {
         public let time: TimeInterval
-        public let title: String?
+        public let title: String
     }
 }
