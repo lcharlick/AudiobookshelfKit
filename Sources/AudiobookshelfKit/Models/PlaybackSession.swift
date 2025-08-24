186b910bb09476b29aee053aafefd4112f2c7121
Lachlan Charlick <lachlan.charlick@gmail.com>
2025-08-24 20:13:32 +0930

    Optional PlaybackSession properties

--- Sources/AudiobookshelfKit/Models/PlaybackSession.swift
+++ Sources/AudiobookshelfKit/Models/PlaybackSession.swift
@@ -12,19 +12,15 @@ public struct PlaybackSession: Codable, Hashable, Identifiable, Sendable {
     /// The ID of the playback session.
     public let id: String
     /// The ID of the user the playback session is for.
-    public let userId: String
+    public let userId: String?
     /// The ID of the library that contains the library item.
-    public let libraryId: String
+    public let libraryId: String?
     /// The ID of the library item.
     public let libraryItemId: String
     /// The ID of the podcast episode. Will be null if this playback session was started without an episode ID.
     public let episodeId: String?
     /// The media type of the library item. Will be book or podcast.
     public let mediaType: String
-    /// The metadata of the library item's media.
-    public let mediaMetadata: LibraryItemExpanded.Book.MediaMetadata?
-    /// The chapters of the library item.
-    public let chapters: [Chapter]?
     /// The title of the playing item to show to the user.
     public let displayTitle: String?
     /// The author of the playing item to show to the user.
@@ -34,7 +30,7 @@ public struct PlaybackSession: Codable, Hashable, Identifiable, Sendable {
     /// The total duration (in seconds) of the playing item.
     public let duration: TimeInterval?
     /// What play method the playback session is using. See below for values.
-    public let playMethod: PlayMethod
+    public let playMethod: PlayMethod?
     /// The given media player when the playback session was requested.
     public let mediaPlayer: String?
     /// The given device info when the playback session was requested.
